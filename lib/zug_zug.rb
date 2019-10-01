cf18469b9ba231b0f715c453356a63763b18c3b8
Ronan Limon Duparcmeur <ronan@2-45.pm>
2019-10-01 14:36:36 +0200

    First commit
    A brand new gem template, as generated by Bundler (2.0.2)

---
+++ lib/zug_zug.rb
@@ -0,0 +1,6 @@
+require "zug_zug/version"
+
+module ZugZug
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/zug_zug_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class ZugZugTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::ZugZug::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
